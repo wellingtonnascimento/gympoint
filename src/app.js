5fcce543d326abb5b28a314a462189a928c4a675
Wellington N <wellingto_07@hotmail.com>
2020-07-01 09:05:16 -0300

    create loader sequelize

--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
 import express from 'express';
 import routes from './routes';
 
-impo;
+import './database';
+
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;

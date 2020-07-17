2e01c51fea16271a578ff02c1b2b776f1fe976ae
Wellington N <wellingto_07@hotmail.com>
2020-07-17 15:19:30 -0300

    correção

--- src/app/controllers/StudentsController.js
+++ src/app/controllers/StudentsController.js
@@ -37,6 +37,7 @@ class StudentsController {
   }
 
   async update(req, res) {
+    const { id } = req.params;
     const schema = Yup.object().shape({
       name: Yup.string(),
       email: Yup.string().email(),
@@ -48,8 +49,6 @@ class StudentsController {
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails.' });
     }
-
-    const { id } = req.params;
 
     const studentExists = await Students.findOne({ where: { id } });
 
@@ -63,7 +62,7 @@ class StudentsController {
       });
 
       if (studentEmailExists) {
-        return res.status(400).json({ error: 'Student already exists.' });
+        return res.status(400).json({ error: 'Student already email exists.' });
       }
     }
 
@@ -80,44 +79,6 @@ class StudentsController {
       altura,
     });
   }
-
-  // async update(req, res) {
-  //   const schema = Yup.object().shape({
-  //     name: Yup.string(),
-  //     email: Yup.string(),
-  //     idade: Yup.number(),
-  //     peso: Yup.number().positive().max(250),
-  //     altura: Yup.number().positive(),
-  //   });
-
-  //   if (!(await schema.isValid(req.body))) {
-  //     return res.status(400).json({ error: 'Validation fails' });
-  //   }
-
-  //   const { email } = req.body;
-
-  //   const students = await Students.findByPk(req.studentsId);
-
-  //   if (email !== students.email) {
-  //     const studentsExists = await Students.findOne({
-  //       where: { email },
-  //     });
-
-  //     if (studentsExists) {
-  //       return res.status(400).json({ error: 'Students already exists' });
-  //     }
-  //   }
-
-  //   const { name, idade, peso, altura } = await students.update(req.body);
-
-  //   return res.json({
-  //     name,
-  //     email,
-  //     idade,
-  //     peso,
-  //     altura,
-  //   });
-  // }
 }
 
 export default new StudentsController();

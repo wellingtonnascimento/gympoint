b7c8355f58a434899c6f7fd300bebabf6a554d0f
Wellington N <wellingto_07@hotmail.com>
2020-07-17 11:40:29 -0300

    create route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import StudentsController from './app/controllers/StudentsController';
+import PlansController from './app/controllers/PlansController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -14,5 +15,7 @@ routes.use(authMiddleware);
 
 routes.post('/students', StudentsController.store);
 routes.put('/students/:id', StudentsController.update);
+
+routes.post('/plans', PlansController.store);
 
 export default routes;

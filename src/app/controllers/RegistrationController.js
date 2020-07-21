b8eb89ec658504160b09445393087af1b2278ec9
Wellington N <wellingto_07@hotmail.com>
2020-07-21 14:04:01 -0300

    registration create

--- src/app/controllers/RegistrationController.js
+++ src/app/controllers/RegistrationController.js
@@ -1,8 +1,9 @@
 import * as Yup from 'yup';
-import { addMonths, parseISO, isBefore, isAfter, endOfDay } from 'date-fns';
+import { parseISO, addDays, format } from 'date-fns';
+// import pt from 'date-fns/locale/pt';
 import Registration from '../models/Registration';
-import Plan from '../models/Plans';
-import Student from '../models/Students';
+import Plan from '../models/Plan';
+import Student from '../models/Student';
 
 import ConfirmationMail from '../jobs/ConfirmationMail';
 import Queue from '../../lib/Queue';
@@ -10,85 +11,71 @@ import Queue from '../../lib/Queue';
 class RegistrationController {
   async store(req, res) {
     const schema = Yup.object().shape({
-      start_date: Yup.date().required(),
-      plan_id: Yup.number().required(),
       student_id: Yup.number().required(),
+      plan_id: Yup.number().required(),
+      start_date: Yup.date().required(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'validation fails' });
     }
 
-    const { start_date, plan_id, student_id } = req.body;
-    const parsedStartDate = parseISO(start_date);
+    const { student_id, plan_id, start_date } = req.body;
 
-    const checkPlanExists = await Plan.findByPk(plan_id);
-
-    if (!checkPlanExists) {
-      return res.status(401).json({ error: 'Plan not found' });
+    if (!plan_id || !student_id) {
+      return res
+        .status(401)
+        .json({ error: 'Plan ID or Student ID is invalid.' });
     }
 
-    const checkStudentExists = await Student.findByPk(student_id);
-
-    if (!checkStudentExists) {
-      return res.status(401).json({ error: 'Student not found' });
-    }
-
-    const checkStudentHasRegistration = await Registration.findOne({
-      where: {
-        student_id,
-      },
+    const registrationExists = await Registration.findOne({
+      where: { student_id },
     });
 
-    if (
-      checkStudentHasRegistration &&
-      (checkStudentHasRegistration.active ||
-        isAfter(
-          endOfDay(checkStudentHasRegistration.start_date),
-          endOfDay(new Date())
-        ))
-    ) {
+    if (registrationExists) {
       return res
-        .status(400)
-        .json({ error: 'Student already has a active membership' });
+        .status(401)
+        .json({ error: 'A registration with this student already exists' });
     }
 
-    if (isBefore(endOfDay(parsedStartDate), new Date())) {
-      return res.status(400).json({ error: 'Past dates are not permitted' });
+    const planExists = await Plan.findOne({ where: { id: plan_id } });
+
+    if (!planExists) {
+      return res.status(400).json({ error: 'Plan not found.' });
     }
 
-    const end_date = addMonths(parsedStartDate, checkPlanExists.duration);
-    const price = checkPlanExists.price * checkPlanExists.duration;
+    const studentExists = await Student.findOne({
+      where: { id: student_id },
+    });
+
+    if (!studentExists) {
+      return res.status(400).json({ error: 'Student not found.' });
+    }
+
+    const { duration, price } = planExists;
+
+    const end_date = format(
+      addDays(parseISO(start_date), duration * 30),
+      'yyyy-MM-dd'
+    );
 
     const registration = await Registration.create({
       student_id,
       plan_id,
-      start_date: parsedStartDate,
+      start_date,
+      end_date,
+      price: price * duration,
+    });
+
+    await Queue.add(ConfirmationMail.key, {
+      studentExists,
+      planExists,
+      start_date,
       end_date,
       price,
     });
-    const registrationInfo = await Registration.findByPk(registration.id, {
-      include: [
-        {
-          model: Student,
-          as: 'student',
-          attributes: ['id', 'name', 'email'],
-        },
-        {
-          model: Plan,
-          as: 'plan',
-          attributes: ['title'],
-        },
-      ],
-    });
 
-    if (process.env.NODE_ENV !== 'test') {
-      await Queue.add(ConfirmationMail.key, {
-        registrationInfo,
-      });
-    }
-
-    return res.json(registrationInfo);
+    return res.json(registration);
   }
 }
 
